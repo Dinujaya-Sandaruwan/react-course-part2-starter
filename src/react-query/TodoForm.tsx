892deabc73a4b435b63c259a9080cd2d83943671
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-06-29 03:43:54 +0530

    17-_Showing_Mutation_Progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -12,7 +12,7 @@ interface Todo {
 
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation({
+  const addTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
@@ -27,37 +27,46 @@ const TodoForm = () => {
         savedTodo,
         ...(todos || []),
       ]);
+
+      if (ref.current) ref.current.value = "";
     },
   });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form
-      className="row mb-3"
-      onSubmit={(event) => {
-        event.preventDefault();
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
+      <form
+        className="row mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
 
-        // if (!ref.current?.value) {
-        //   alert("Please enter a todo");
-        //   return;
-        // }
+          // if (!ref.current?.value) {
+          //   alert("Please enter a todo");
+          //   return;
+          // }
 
-        if (ref.current && ref.current?.value)
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            completed: false,
-            userId: 1,
-          });
-      }}
-    >
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+          if (ref.current && ref.current?.value)
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              completed: false,
+              userId: 1,
+            });
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Saving..." : "Add Todo"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
