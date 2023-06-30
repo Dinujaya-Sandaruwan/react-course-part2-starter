022e0844d27abc94d31a2b2320af641e39838447
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-06-30 06:44:47 +0530

    18-_Optimistic_Updates

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,7 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useRef } from "react";
-// import { Todo } from "./hooks/useTodos";
 
 interface Todo {
   id: number;
@@ -10,27 +9,49 @@ interface Todo {
   completed: boolean;
 }
 
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo>({
-    mutationFn: (todo: Todo) =>
-      axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
-        .then((res) => res.data),
-
-    onSuccess: (savedTodo, newTodo) => {
-      console.log(savedTodo);
-
-      // Updating the data in cache
-
-      queryClient.setQueriesData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
-        ...(todos || []),
-      ]);
-
-      if (ref.current) ref.current.value = "";
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>(
+    async (todo: Todo) => {
+      const response = await axios.post<Todo>(
+        "https://jsonplaceholder.typicode.com/todos",
+        todo
+      );
+      return response.data;
     },
-  });
+    {
+      onMutate: (newTodo: Todo) => {
+        const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+
+        // Updating the data in cache
+        queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+          newTodo,
+          ...(todos || []),
+        ]);
+
+        if (ref.current) {
+          ref.current.value = "";
+        }
+
+        return { previousTodos };
+      },
+      onSuccess: (savedTodo, newTodo) => {
+        queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+          todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+        );
+      },
+      onError: (error, newTodo, context) => {
+        if (!context) return;
+
+        queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
+      },
+    }
+  );
+
   const ref = useRef<HTMLInputElement>(null);
 
   return (
@@ -43,18 +64,19 @@ const TodoForm = () => {
         onSubmit={(event) => {
           event.preventDefault();
 
-          // if (!ref.current?.value) {
-          //   alert("Please enter a todo");
-          //   return;
-          // }
+          if (!ref.current?.value) {
+            alert("Please enter a todo");
+            return;
+          }
 
-          if (ref.current && ref.current?.value)
+          if (ref.current && ref.current.value) {
             addTodo.mutate({
               id: 0,
-              title: ref.current?.value,
+              title: ref.current.value,
               completed: false,
               userId: 1,
             });
+          }
         }}
       >
         <div className="col">
